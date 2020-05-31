cf338210cda36b945aa06cd5896487e8c6454d08
shantam sultania <48251619+shantamsultania@users.noreply.github.com>
2020-05-31 16:54:25 +0530

    Update facelock2.py

--- facelock2.py
+++ facelock2.py
@@ -2,75 +2,55 @@ import cv2
 import numpy as np
 from os import listdir
 from os.path import isfile, join
-
-face0 =cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-data_path = './datacollection/'
-
-onlyfiles = [f for f in listdir(data_path) if isfile(join(data_path,f))]
-
+face0 = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+data_path = '../datacollection/'
+onlyfiles = [f for f in listdir(data_path) if isfile(join(data_path, f))]
 Training_Data, Labels = [], []
-
 for i, files in enumerate(onlyfiles):
     image_path = data_path + onlyfiles[i]
     images = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
     Training_Data.append(np.asarray(images, dtype=np.uint8))
     Labels.append(i)
-
 Labels = np.asarray(Labels, dtype=np.int32)
-
 model = cv2.face.LBPHFaceRecognizer_create()
-
 model.train(np.asarray(Training_Data), np.asarray(Labels))
-
-print("Model Training.......")
-
-def face_detector(img, size = 0.5):
+print("Model Trained ")
+def face_detector(img, size=0.5):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face0.detectMultiScale(gray,1.5,5)
-
-    if faces is():
-        return img,[]
-
-    for(x,y,w,h) in faces:
-        cv2.rectangle(img, (x,y),(x+w,y+h),(0,255,255),2)
-        roi = img[y:y+h, x:x+w]
-        roi = cv2.resize(roi, (200,200))
-
-    return img,roi
-
+    faces = face0.detectMultiScale(gray, 1.5, 5)
+    if faces is ():
+        return img, []
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 255), 2)
+        roi = img[y:y + h, x:x + w]
+        roi = cv2.resize(roi, (200, 200))
+    return img, roi
 cap = cv2.VideoCapture(0)
-
+print("Starting FACE LOCK")
 while True:
-
     ret, frame = cap.read()
-
     image, face = face_detector(frame)
-
     try:
         face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
         result = model.predict(face)
         if result[1] < 500:
-            confidence = int(100*(1-(result[1])/300))
-            display_string = str(confidence)+'% Confidence it is user'
-        cv2.putText(image,display_string,(100,120), cv2.FONT_HERSHEY_COMPLEX,1,(250,120,255),2)
+            confidence = int(100 * (1 - (result[1]) / 300))
+            display_string = str(confidence) + '% Confidence it is user'
+            cv2.putText(image, display_string, (100, 120), cv2.FONT_HERSHEY_COMPLEX, 1, (250, 120, 255), 2)
 
-        if confidence > 80:
+        if confidence > 75:
             cv2.putText(image, "Unlocked", (250, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
             cv2.imshow('Face Cropper', image)
 
         else:
             cv2.putText(image, "Locked", (250, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
-             display_string = str(confidence) + '% Confidence it is not user'
+            # display_string = str(confidence) + '% Confidence it is not user'
             cv2.imshow('Face Cropper', image)
     except:
         cv2.putText(image, "Face Not Found", (250, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
         cv2.imshow('Face Cropper', image)
         pass
-
-    if cv2.waitKey(1)== 27:
+    if cv2.waitKey(1) == 27:
         break
-
-
 cap.release()
 cv2.destroyAllWindows()
